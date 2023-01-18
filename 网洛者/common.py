6e9105946160f573858aa1e51c7a15ec3e86cbc5
Heartfilia <lodge6410@vip.qq.com>
2023-01-18 12:26:31 +0800

    第七题还没做

--- 网洛者/common.py
+++ 网洛者/common.py
@@ -7,41 +7,51 @@ from loguru import logger
 from lite_tools import get_ua, try_get
 
 
-root = os.path.dirname(__file__)
+root = os.path.dirname(__file__)  # 需要在 common.py 同级目录创建一个 config.json 配置文件 内容是 {"cookie": "你的页面cookie"}
 
 
-def match_run(timu: int, cal_js: Callable = None, other_headers: dict = None):
+def __each_page(challenge: int, cal_js: Callable = None, other_headers: dict = None, page: int = 1) -> int:
+    url = f'http://spider.wangluozhe.com/challenge/api/{challenge}'
+    payload = {"page": page, "count": 10}
+    if cal_js is not None:
+        sign = cal_js()
+        payload.update({"_signature": sign})
+    base_headers = {
+        "user-agent": get_ua(),
+        "Cookie": try_get(os.path.join(root, "config.json"), "cookie", options={"mode": "file"}),
+        "Host": "spider.wangluozhe.com",
+        "Origin": "http://spider.wangluozhe.com",
+        "Referer": f"http://spider.wangluozhe.com/challenge/{challenge}"
+    }
+    if other_headers:
+        for header_key, header_value in other_headers.items():
+            if header_key in base_headers:  # 这变动只会出现在cookie部分 在原cookie后面追加字符串cookie
+                base_headers[header_key] += f"; {header_value}"
+            else:
+                base_headers[header_key] = header_value
+    resp = requests.post(
+        url,
+        data=payload,
+        headers=base_headers
+    )
+    data = resp.json()
+    if not data:
+        return 0
+    logger.debug(f"题目:[{challenge}] 页码:[{page:>2}] 结果:{data}")
     value = 0
-    for i in range(1, 101):
-        url = f'http://spider.wangluozhe.com/challenge/api/{timu}'
-        payload = {"page": i, "count": 10}
-        if cal_js is not None:
-            sign = cal_js()
-            payload.update({"_signature": sign})
-        base_headers = {
-            "user-agent": get_ua(),
-            "Cookie": f'session={try_get(os.path.join(root, "config.json"), "session", options={"mode": "file"})}',
-            "Host": "spider.wangluozhe.com",
-            "Origin": "http://spider.wangluozhe.com",
-            "Referer": f"http://spider.wangluozhe.com/challenge/{timu}"
-        }
-        if other_headers:
-            for header_key, header_value in other_headers.items():
-                if header_key in base_headers:  # 这变动只会出现在cookie部分
-                    origin_value = base_headers[header_key]
-                    base_headers[header_key] = origin_value + f"; {header_value}"
-                else:
-                    base_headers[header_key] = header_value
-        resp = requests.post(
-            url, 
-            data=payload, 
-            headers=base_headers
-        )
-        data = resp.json()
-        if not data:
-            break
-        logger.debug(f"题目:[{timu}] 页码:[{i:>2}] 结果:{data}")
-        for item in data['data']:
-            value += item['value']
+    for item in data['data']:
+        value += item['value']
+    return value
+
+
+def match_run(challenge: int, cal_js: Callable = None, other_headers: dict = None, page: int = None):
+    value = 0
+    if page:
+        value = __each_page(challenge, cal_js, other_headers, page)   # 指定页码
+        logger.success(f"第[{page}]页单独 计算结果是: {value}")
+        return value
+
+    for page in range(1, 101):
+        value += __each_page(challenge, cal_js, other_headers, page)  # 不指定页码会翻全部页码并统计
 
     logger.success(f"计算结果是: {value}")
--- 网洛者/第六题/six.py
+++ 网洛者/第六题/six.py
@@ -1,11 +1,25 @@
 import sys
-# import execjs
-from lite_tools import get_time
+import execjs
+
 sys.path.append('../')
 from common import match_run
 
-# with open('base.js', 'r', encoding='utf-8') as fp:
-#     js = fp.read()
-# jsx = execjs.compile(js)
+with open('base.js', 'r', encoding='utf-8') as fp:
+    js = fp.read()
+jsx = execjs.compile(js)
 
-match_run(6)
+
+def get_hexin():
+    """还没有实现这里"""
+    return jsx.call("")
+
+
+# 这个题用jsdom可以做出来 但是我不想用jsdom  所以直接贴了一个固定值 后面补环境学完了会重新来弄这里
+value = 0
+
+for page in range(1, 101):
+    value += match_run(6, other_headers={"hexin-v": get_hexin()}, page=page)
+
+print(f"最终结果是: {value}")
+
+# match_run(6, other_headers={"hexin-v": "AwxFPk7mgFXNsZfkSCyDkns43WE7RbDvsunEs2bNGLda8aJfjlWAfwL5lEG1"})  # 投机取巧的
